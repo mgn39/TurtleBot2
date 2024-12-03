f41fc3cdf0429ce3e3f961ba472bbf82d5a992a9
kaleb <kalebkim1939@gmail.com>
2024-12-02 22:56:45 -0500

    oh boi

--- Kaleb/Week 14.md
+++ Kaleb/Week 14.md
@@ -1,10 +1,47 @@
-## <u>11/27/24</u>
+## <u>12/2/24</u>
+It's not Sunday, but here we are.
+#### Modularity
+On the desktop (master), uploading the `kobuki_ws` to a repo at [https://github.com/SaPHaRI-Lab/Turtlebot2-kobuki_ws](https://github.com/SaPHaRI-Lab/Turtlebot2-kobuki_ws).
 
-### The System (Day 3)
+While doing this initially, I accidentally deleted everything and had to recreate the folder. Sorry Chester.
 
-#### Web Server
-Switching from HTTP to MQTT (Message Querying Telemetry Transport).
+There exists repos in this repo, so I went into every folder and ran `rm -rf .git` to remove repo data. To work with Git, these are my steps:
+- Ensure Git's installed already. If it isn't, run `sudo apt-get install git-all`
+- Navigate to `kobuki_ws`, then run:
+- `git init`
+- `git remote add origin https://github.com/SaPHaRI-Lab/Turtlebot2-kobuki_ws`
+- `git add .`
+- `echo "# Turtlebot 2 blah blah blah > README.md`
+- If you need a `.gitignore`, use:
+	- `vim .gitignore` (then edit)
+	- `git rm --cached .` or `git reset`
+- `git commit -m "init"`
+- Now the slightly annoying part: authentication
+	- I'm using [Git Credential Manager](https://github.com/git-ecosystem/git-credential-manager) (GCM) to simplify the auth process.
+	- Test if you have .NET by running `dotnet` in the terminal. If not found, I installed [.NET here](https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?tabs=dotnet9&pivots=os-linux-ubuntu-2004) since I dislike `snap`
+	- Then follow the [.NET Linux section](https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/install.md#net-tool)–a restart is required!
+	- `git-credential-manager configure`
+	- Since we're on Linux with a GUI, we'll use secretservice. Run `export GCM_CREDENTIAL_STORE=secretservice`
+- Config auth
+	- `git config --global user.email <email>`
+	- `git config --global user.name <name>`
+	- Check with `cat ~/.gitignore`, if need to reset delete the file with `rm`
+- Finally, `git push --set-upstream origin master`
 
-Will use the Jetson on Catherine Parr, aka Turtlebot 6, as the Turtlebot manager and will receive signals from the ESP32.
+### Communication (Day 2)
+Switching from HTTP to [MQTT](https://randomnerdtutorials.com/what-is-mqtt-and-how-it-works/) (Message Querying Telemetry Transport).
 
-I'm going to Chicago so I'll be back on Sunday
+Was originally going to use the Jetson, but for compatibility and convenience we'll be using the Desktop that's also the master for the Turtlebots.
+
+**Goal:** We need the ESP32 to send messages to the Desktop (our broker) over WiFi via MQTT, then a response is sent both to the ESP32 and a corresponding Turtlebot.
+
+Modified `main.cpp` for the ESP32 again, with comments explaining key parts of the code. 
+
+For the Desktop, we need to use **Mosquitto** for a broker. Install with `sudo apt install mosquitto mosquitto-clients`. It should run automatically, but it can be manually started with `sudo systemctl start mosquitto` and `sudo systemctl enable mosquitto`.
+
+For the Pis, I've created a fork of `minimal.launch` to `minimal_ext.launch` that incorporates the script `turtlebot_mqtt`. It'll be using the **Paho MQTT** client. It's available in [this directory of the repo](https://github.com/SaPHaRI-Lab/Turtlebot2-kobuki_ws/tree/master/src/turtlebot/turtlebot_bringup/scripts) created just earlier. This means we'll need to run `minimal_ext.launch` instead of `minimal.launch` for the Pis moving forward.
+
+TODO: Haven't tested for any device. `main.cpp` should work and I'll test it using the mosquitto client on the Desktop. Then, I'll test it on a Turtlebot with a Pi to send a command to move the Turtlebot forward.
+## <u>12/3/24</u>
+### Communication (Day 3)
+for custom, deployed movement of independant bot swarms, 
--- src/Eminem/src/main.cpp
+++ src/Eminem/src/main.cpp
@@ -3,6 +3,7 @@
 #include <RCSwitch.h>
 #include <WiFi.h>
 #include <string>
+#include <PubSubClient.h>
 
 // GPIO Pins
 #define LED 13
@@ -14,172 +15,111 @@
 // Network Credentials
 #define SSID "G519A"
 #define PASSWORD "Blocklab519a"
+// MQTT Broker Settings
+#define MQTT_BROKER "192.168.1.41"   // broker is desktop
+#define MQTT_PORT 1883               // unencrypted port (8883 if encrypted later on)
 
 int setup_server(const char *ssid, const char *pass);
 int run_server();
 
-// servo
+// Servo
 Servo servo;
 int pos = 0;
 int ADC_MAX = 4096; // default ADC max value; ESP32s have a 12-bit ADC width opposed to Arduinos' 10-bit
+// RCSwitch rfReceiver = RCSwitch(); // RF Receiver
 
-// RF receiver
-RCSwitch mySwitch = RCSwitch();
+// WiFi and MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+bool activated = false;
+const char* mqtt_topic_sub = "esp32/motor";
+const char* mqtt_topic_pub = "esp32/doorbell";
 
-// web server
-WiFiServer server(80); // init on port 80
-String header; // request header
+// Function Prototypes
+void setup_wifi();
+void mqtt_callback(char* topic, byte* message, unsigned int length);
+void reconnect();
 
-unsigned long currentTime = millis();
-unsigned long previousTime = 0;
-const long timeoutConnectTime = 15000; // in ms
-const long timeoutTime = 2000; // in ms
-
-bool motorState = false;
-
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     pinMode(LED, OUTPUT);
     servo.attach(SERVO, SERVOMIN, SERVOMAX);
     pinMode(RF, INPUT);
-    mySwitch.enableReceive(digitalPinToInterrupt(RF));
+    // rfReceiver.enableReceive(digitalPinToInterrupt(RF));
     digitalWrite(LED, HIGH); // turn on LED
 
     // WiFi connection
-    setup_server(SSID, PASSWORD);
+    setup_wifi();
+    client.setServer(MQTT_BROKER, MQTT_PORT);
+    client.setCallback(mqtt_callback);
 }
 
-void loop()
-{
-    // if (!moving && run_server() == 0) {
-    //     Serial.println("Activating motor...");
-    //     moving = true;
-    // }
-    run_server();
-    
-    // Switch
-    Serial.println(digitalRead(21));
-    if (mySwitch.available())
-    {
-        Serial.println("RF switch detected!");
-        int value = mySwitch.getReceivedValue();
-        if (value == 0)
-        {
-            Serial.println("Unknown signal");
-        }
-        else
-        {
-            Serial.print("Received value: ");
-            Serial.println(value);
-        }
-        mySwitch.resetAvailable();
+void loop() {
+     // Ensure MQTT stays connected
+    if (!client.connected()) {
+        reconnect();
     }
-    
-    // Motor
-    if (motorState) {
+    client.loop();
+    client.publish(mqtt_topic_pub, "this is the msg from rf receiver");
+
+    // Motor Logic
+    if (activated) {
         digitalWrite(LED, HIGH);
         for (pos = 0; pos <= 180; pos += 1) {
             servo.write(pos);
             delay(10);
         }
         digitalWrite(LED, LOW);
-        motorState = false;
+        activated = false;
     }
 }
 
-int setup_server(const char *ssid, const char *pass)
-{
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, pass);
-    currentTime = millis();
-    previousTime = currentTime;
-    while (WiFi.status() != WL_CONNECTED && currentTime - previousTime <= timeoutConnectTime)
-    {
+// Connect to WiFi
+void setup_wifi() {
+    Serial.print("Connecting to WiFi...");
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
-        currentTime = millis();
-        if (currentTime - previousTime > timeoutConnectTime) {
-            Serial.println("[ERROR] Cannot connect to WiFi.");
-            return 1;
-        }
     }
-    Serial.print("Successfully connected to WiFi! \n IP address: ");
-    Serial.println(WiFi.localIP());
-    server.begin();
-    return 0;
+    Serial.println("Connected to WiFi!");
 }
 
-int run_server() {
-    WiFiClient client = server.available();   // Listen for incoming clients
-    if (client) {                             // If a new client connects,
-        currentTime = millis();
-        previousTime = currentTime;
-        Serial.println("New Client.");          // print a message out in the serial port
-        String currentLine = "";                // make a String to hold incoming data from the client
-        while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-            currentTime = millis();
-            if (client.available()) {             // if there's bytes to read from the client,
-                char c = client.read();             // read a byte, then
-                Serial.write(c);                    // print it out the serial monitor
-                header += c;
-                if (c == '\n') {                    // if the byte is a newline character
-                    // if the current line is blank, you got two newline characters in a row.
-                    // that's the end of the client HTTP request, so send a response:
-                    if (currentLine.length() == 0) { // here we send an entire HTML response
-                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                        // and a content-type so the client knows what's coming, then a blank line:
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-type:text/html");
-                        client.println("Connection: close");
-                        client.println();
-                        
-                        // Turn GPIO on/off
-                        if (header.indexOf("GET /actuate") >= 0 && !motorState) {
-                            Serial.println("actuating...");
-                            motorState = true;
-                        }
-                        
-                        // HTML Head
-                        client.println("<!DOCTYPE html><html>");
-                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        client.println("<link rel=\"icon\" href=\"data:,\">");
-                        // CSS to style the on/off buttons
-                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-                        client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-                        client.println(".button2 {background-color: #555555;}</style></head>");
+// MQTT Message Callback
+void mqtt_callback(char* topic, byte* message, unsigned int length) {
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    String messageTemp;
 
-                        // HTML Body
-                        client.println("<body><h1>ESP32 Web Server</h1>");
-                        // Display current state and ON/OFF buttons
-                        client.println("<p>Actuated: " + String(motorState) + "</p>");   
-                        if (motorState) {
-                            client.println("<p><a href=\"/26/off\"><button class=\"button\">ON</button></a></p>");
-                        } else {
-                            client.println("<p><a href=\"/26/on\"><button class=\"button button2\">OFF</button></a></p>");
-                        } 
-                        
-                        // The HTTP response ends with another blank line
-                        client.println();
-                        break;
-                    } else { // if you got a newline, then clear currentLine
-                        currentLine = "";
-                    }
-                } else if (c != '\r') {  // if you got anything else but a carriage return character,
-                    currentLine += c;      // add it to the end of the currentLine
-                }
-            }
+    for (int i = 0; i < length; i++)
+        messageTemp += (char)message[i];
+    Serial.println(messageTemp);
+
+    // Motor control
+    if (String(topic) == mqtt_topic_sub) {
+        if (messageTemp == "on") {
+            Serial.println("Turning motor ON");
+            activated = true;
+        } else if (messageTemp == "off") {
+            Serial.println("Turning motor OFF");
+            activated = false;
         }
-        // Clear the header variable
-        header = "";
-        // Close the connection
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
-    } else {
-        return 1;
     }
-    return 0;
+}
+
+// Reconnect to MQTT Broker
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("ESP32Client")) {
+            Serial.println("connected");
+            client.subscribe(mqtt_topic_sub); // Subscribe to the control topic
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
 }
